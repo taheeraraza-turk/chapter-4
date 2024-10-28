94ab7165971fcb639092a14573ee361c5f59f5c2
Taheera Raza Turk <taheeraraza0@gmail.com>
2024-10-28 10:37:00 -0700

    Add files via upload

---
+++ chapter 4/JS/app.js
@@ -0,0 +1,24 @@
+// 1. Declare 3 variables in one statement
+      let one; let two; let three;
+
+// 2. Declare 5 legal & 5 illegal variable names
+    //  Legal varaibles
+    // let name;
+    // let my_name;
+    // let $dollar;
+    // let myName;
+    // let _number1;
+
+    // Illegal variables
+    // let 75name;
+    // let my name;
+    // let my-name;
+    // let var;
+    // let #hi;
+
+// 3.Display this in your browser.
+     document.write("<bold> ❗ RULES FOR NAMING JS VARIABLES ❗ <br> <br>");
+     document.write(" ◽ Variable names can only contain NUMBERS, $ and UNDERSCOPE.<br> For Example:e $my_1stVariable <br> ");
+     document.write(" ◽ Variables must begin with a LETTER , $ OR UNDERSCOPE <br> For example: $name, _name or name <br>");
+     document.write(" ◽ Variables names are case sensitive <br>");
+     document.write(" ◽ Variables names should not be JS keywords <br>");
